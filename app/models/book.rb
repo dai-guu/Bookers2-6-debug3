78eb10d737ebbdd6ac1f91e041f06b6ac2bdd938
dai-dai <daiguu2021@gmail.com>
2021-09-27 04:48:19 +0000

    [Finish] 4-10

---
+++ app/models/book.rb
@@ -0,0 +1,17 @@
+class Book < ApplicationRecord
+  
+
+    belongs_to :user #Userモデルと1：N
+   
+
+   
+
+    validates :title, presence: true
+    validates :body, presence: true,
+              length: { maximum: 200 }
+   
+  
+
+end
+  
+
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '72de80cbb5aead5481d1c0edab318e4830c65d9d0d1f669833e83bfb3ee4257e5555398fdc1468929977c8d6646a3f091ea91ad65ad565c352dd0171041a1474'
