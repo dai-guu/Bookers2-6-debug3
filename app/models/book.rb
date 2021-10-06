35facdf194072256e2bfcd8a577744bac4287adb
dai-dai <daiguu2021@gmail.com>
2021-10-06 02:39:39 +0000

    [Finish]応用課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,6 +11,27 @@ class Book < ApplicationRecord
      end
 
 
+
+     def self.looks(word, search)   #(search, word)にしない controllerと順番合わせる
+         @book = Book.all
+       if search == "perfect_match"
+
+         @book = Book.where("title LIKE?","#{word}")      
+       elsif search == "forward_match"
+         @book = Book.where("title LIKE?","#{word}%")       
+       elsif search == "backward_match"
+         @book = Book.where("title LIKE?","%#{word}")
+       elsif search == "partial_match"
+         @book = Book.where("title LIKE?","%#{word}%")
+       else
+         @book        #rb:16で定義してるから=Book.allはいらない
+       end
+    end
+
+
+
+
+
     validates :title, presence: true
     validates :body, presence: true,
               length: { maximum: 200 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,24 @@ class User < ApplicationRecord
 
 
 
+    def self.looks(word, search)  # controller:(params[:word], params[:search]) と順番統一させる
+      @user = User.all            # @userを定義
+      if search == "perfect_match"                 #完全一致 ％いらない
+        @user.where("name LIKE?", "#{word}")
+      elsif search == "forward_match"
+        @user.where("name LIKE?", "#{word}%")    # %の位置で前後を区切ってる
+      elsif search == "backward_match"
+        @user.where("name LIKE?", "%#{word}")
+      elsif search == "partial_match"
+        @user.where("name LIKE?", "%#{word}%")    #部分一致だから％で囲む
+      else
+        @user  #上のrb:46で定義したから =User.allはいらない
+      end
+    end
+
+
+
+
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる
   validates :name, uniqueness: true
   validates :name, length: {maximum: 20, minimum: 2}
