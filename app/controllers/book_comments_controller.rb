a3aece7deac4db5355a62a2483018e6b3227c63b
dai-dai <daiguu2021@gmail.com>
2021-10-08 10:53:47 +0000

    [Continue]Oyou6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,16 +6,17 @@ class BookCommentsController < ApplicationController
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
-   unless @book_comment.save
-    render 'error'
-   end
+    @book_comment.save
+
+
+
  end
 
 
   def destroy
    @book = Book.find(params[:book_id])
-    book_comment = @book.book_comments.find(params[:id])
-    book_comment.destroy
+   @book_comment = BookComment.find_by(params[:book_id])
+   @book_comment.destroy
   end
 
   private
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -2,9 +2,9 @@ class BookComment < ApplicationRecord
 
  belongs_to :user
   belongs_to :book
-  
+
   #バリデーション
-  validates :content, presence: true
+  #validates :content, presence: true
 
 
 end
